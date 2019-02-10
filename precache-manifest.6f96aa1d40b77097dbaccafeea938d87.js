54f9808dce8dca08afae5efef96ccd2b9723355c
CircleCI (on behalf of Christer Edvartsen) <cogo@starzinger.net>
2019-02-10 08:54:40 +0000

    Deployment to GitHub Pages from CircleCI: 78a89dea18671f11cffb2584a61a0280be130d8f

---
+++ precache-manifest.6f96aa1d40b77097dbaccafeea938d87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de351821a5ed7651416b",
+    "url": "/black-jack/static/js/runtime~main.de351821.js"
+  },
+  {
+    "revision": "06daac73c3498dd52dc0",
+    "url": "/black-jack/static/js/main.06daac73.chunk.js"
+  },
+  {
+    "revision": "80eefc3c594cc732b347",
+    "url": "/black-jack/static/js/1.80eefc3c.chunk.js"
+  },
+  {
+    "revision": "06daac73c3498dd52dc0",
+    "url": "/black-jack/static/css/main.ae3562f1.chunk.css"
+  },
+  {
+    "revision": "6ff3d2c2d3f6a77653e7fcfb30f2fe1b",
+    "url": "/black-jack/index.html"
+  }
+];
